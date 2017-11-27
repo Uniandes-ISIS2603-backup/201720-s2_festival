a7f9680a9dcf869bcb06b074fc0464b6b359650d
David Bautista <dj.bautista10@uniandes.edu.co>
2017-11-27 13:30:28 -0500

    adicion de registros en festivalcine.sql

--- festival-web/src/main/webapp/src/modules/peliculas/masDetalle/peliculaAmpliada.ctrl.js
+++ festival-web/src/main/webapp/src/modules/peliculas/masDetalle/peliculaAmpliada.ctrl.js
@@ -6,21 +6,19 @@
 
 			$http.get(peliculasContext + $state.params.peliculaId).then(function (response) {
 				$scope.pelicula = response.data;
-
 				$scope.trustSrc = function(src) {
 					return $sce.trustAsResourceUrl(src);
 				};
-
-				$scope.calificaciones = new Array();
+				
+				$http.get(peliculasContext + $state.params.peliculaId + '/calificaciones').then(function (response) {
+					$scope.calificaciones = response.data;
+				});
 
 				$http.get(peliculasContext + $state.params.peliculaId + '/funciones').then(function (response) {
-
 					var funciones = response.data;
 					var funcionesReturn = new Array();
-					$scope.rutas = new Array();
+					console.log('FuncionesLenght: ' + funciones.length);
 					for (i = 0; i < funciones.length; i++){
-
-						
 
 						var inicioSplit = funciones[i].horaInicio.split("T");
 						var fecha = inicioSplit[0];
@@ -36,26 +34,9 @@
 							"horaFin" : finSplit[0]
 						};
 						funcionesReturn.push(funcionN);
-
-
-						var rutaCalificaciones = 'api/funciones/' + funciones[i].id + '/calificaciones';
-						$scope.rutas.push(rutaCalificaciones);
-						console.log('1 -> ' + rutaCalificaciones);
 					};
+					console.log('FuncionesRetLenght: ' + funcionesReturn.length);
 					$scope.funcionesRet = funcionesReturn;
-
-
-					console.log('rutas -> ' +  $scope.rutas);
-
-					for(i = 0; i < $scope.rutas.length; i++){
-						var rutaAct = $scope.rutas[i];
-						console.log('Ruta it ' + i + ': ' + rutaAct);
-						$http.get(rutaAct).then(function (response) {
-							var calificacionAct = respose.data;
-							console.log('calificacion Actual: ' + calificacionAct);
-							$scope.calificaciones.push(calificacionAct);
-						});
-					}
 				});
 			});
 		}]);
