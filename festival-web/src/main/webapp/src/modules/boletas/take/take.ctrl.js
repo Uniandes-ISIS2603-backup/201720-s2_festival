0cd6ff26d3b2b43e121dda3f3ab6e3c6f61646a4
Fernando Reyes Bejarano <f.reyes948@uniandes.edu.co>
2017-11-28 10:27:05 -0500

    Compra y reserva de boletas sin manejo de usuario.

---
+++ festival-web/src/main/webapp/src/modules/boletas/take/take.ctrl.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng) {
+    var mod = ng.module("boletasModule");
+    mod.controller('boletas.takeCtrl', ['$scope', '$http', 'boletasContext', '$state',
+        function ($scope, $http, boletasContext, $state) {
+            $scope.take = true;
+            $http.get(boletasContext + $state.params.boletasId).then(function (response) {
+                $scope.boleta = response.data;
+            });
+            $scope.comprarBoletas = function () {
+                $http.put(boletasContext + $state.params.boletasId, {
+                    estado: "COMPRADA",
+                    espectador: {id: $scope.boletaForm.idEspectadorIn.$viewValue}
+                }).then(function (response) {
+                    $state.go('boletasGet', {boletaId: response.data.id});
+                });
+            };
+            $scope.reservarBoletas = function () {
+                $http.put(boletasContext + $state.params.boletasId, {
+                    estado: "RESERVADA",
+                    espectador: {id: $scope.boletaForm.idEspectadorIn.$viewValue}
+                }).then(function (response) {
+                    $state.go('boletasGet', {boletaId: response.data.id});
+                });
+            };
+        }
+
+    ]);
+}
+)(angular);
