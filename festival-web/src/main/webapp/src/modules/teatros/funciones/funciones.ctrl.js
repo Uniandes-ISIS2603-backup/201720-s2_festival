c24ce4e29433c1bdbfa88959f478e2c4dbde7669
David Bautista <dj.bautista10@uniandes.edu.co>
2017-11-29 09:38:21 -0500

    Se quitaron los <Alerts> y se corrigió la conexión entre Teatros y Boletas por función.

--- festival-web/src/main/webapp/src/modules/teatros/funciones/funciones.ctrl.js
+++ festival-web/src/main/webapp/src/modules/teatros/funciones/funciones.ctrl.js
@@ -3,21 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-(function(ng){
-    var mod = ng.module("teatrosModule");
-    mod.controller('teatros.funcionesCtrl',['$scope', '$http', 'teatrosContext', '$state',
-        function($scope, $http, teatrosContext, $state ){
-            console.log("entra a ctrl teatros funcion " + $state.params.teatrosId );
-            $http.get(teatrosContext + $state.params.teatrosId +"/funciones").then(function(response){
-                
-                $scope.funciones = response.data;
-                
-            });
-            
-            
-            
-        }
-    ]);
-}
-)(angular);
+ (function(ng){
+ 	var mod = ng.module("teatrosModule");
+ 	mod.controller('teatros.funcionesCtrl',['$scope', '$http', 'teatrosContext', '$state',
+ 		function($scope, $http, teatrosContext, $state ){
+ 			console.log("entra a ctrl teatros funcion " + $state.params.teatrosId );
+ 			$http.get(teatrosContext + $state.params.teatrosId +"/funciones").then(function(response){
+ 				
+ 				$scope.funciones = response.data;
+ 				$scope.hoverIn = function(){
+ 					this.hoverEdit = true;
+ 				};
+
+ 				$scope.hoverOut = function(){
+ 					this.hoverEdit = false;
+ 				};
+ 				
+ 			});
+ 			
+ 			
+ 			
+ 		}
+ 		]);
+ }
+ )(angular);
 
